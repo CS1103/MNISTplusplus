93bbe47ca93debe1bcc539c674334d57e1ec3426
Roger <rhuauyam@uni.pe>
2023-05-21 12:48:35 -0500

    cleaning out code

--- Google_tests/init/init.cpp
+++ Google_tests/init/init.cpp
@@ -5,15 +5,8 @@ void printHello() {
     std::cout << "Hello, World!\n";
 }
 TEST(HelloWorldTest, PrintHello) {
-// Arrange
-
-// Act
-// Redirect the output stream to capture the output
-testing::internal::CaptureStdout();
-    printHello();  // Assuming the function is defined in your hello_world.cpp file
-
-// Assert
-// Get the captured output and compare with the expected output
-std::string output = testing::internal::GetCapturedStdout();
-EXPECT_EQ(output, "Hello, World!\n");
+    testing::internal::CaptureStdout();
+    printHello();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Hello, World!\n");
 }
