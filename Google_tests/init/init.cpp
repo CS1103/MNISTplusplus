24e648eef5adeed79649bdfb3767581365d6da97
Roger Huauya Mamani <38589877+RogerHuauya@users.noreply.github.com>
2023-05-21 15:43:10 -0500

    gtest integration added (#12)
    * gtest integration added
    
    * cleaning out code
    
    * changing comments
    
    * testing github action
    
    * checkout added
    
    * sudo added
    
    * make changed
    
    * sudo added to make
    
    * test changed
    
    * ctest simplified
    
    * searching
    
    * target added
    
    * new config
    
    * test
    
    * final commit
    
    * test bad code
    
    * final commit

---
+++ Google_tests/init/init.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+void printHello() {
+    std::cout << "Hello, World!\n";
+}
+TEST(HelloWorldTest, PrintHello) {
+    testing::internal::CaptureStdout();
+    printHello();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "Hello, World!\n");
+}
