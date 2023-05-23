5d85fe1df42c433d80ab6b704b7be3a8a78aa38d
arturombc <arturomagno1@gmail.com>
2023-05-22 22:54:56 -0500

    tests done, modified matrix: initialize with 0

--- Google_tests/matrix/test_matrix.cpp
+++ Google_tests/matrix/test_matrix.cpp
@@ -1,72 +1,136 @@
 #include <gtest/gtest.h>
-#include "test_matrix.hpp"
+#include "matrix.h"
 
+TEST(MatrixConstructorTest, ParameterConstructor){
+    Matrix<int> m(3, 3);
+    EXPECT_EQ(m(0,0),0);
+    EXPECT_EQ(m(1,1),0);
+    Matrix<double> m1(3, 3);
+    EXPECT_EQ(m1(0,0), 0.0);
+    EXPECT_EQ(m1(1,1), 0.0);
+}
 
 TEST(MatrixInitializerTest, ConstructorInitializerList){
-    testing::internal::CaptureStdout();
-    test_constructor_initializer_list();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "1 2 3 \n4 5 6 \n7 8 9 \n");
+    Matrix<int> m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    EXPECT_EQ(m(0,0),1);
+    EXPECT_EQ(m(1,1),5);
+    Matrix<double> m1({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    EXPECT_EQ(m1(0,0), 1.0);
+    EXPECT_EQ(m1(1,1), 5.0);
 }
 TEST(MatrixCopyConstructorTest, CopyConstructor){
-    testing::internal::CaptureStdout();
-    test_copy_constructor();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "1 2 3 \n4 5 6 \n7 8 9 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2(m1);
+    EXPECT_EQ(m2(0,0),1);
+    EXPECT_EQ(m2(1,1),5);
+    Matrix<double> m3({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m4(m3);
+    EXPECT_EQ(m4(0,0), 1.0);
+    EXPECT_EQ(m4(1,1), 5.0);
 }
 TEST(MatrixOperatorPlusTest, OperatorPlus){
-    testing::internal::CaptureStdout();
-    test_operator_plus();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "10 10 10 \n10 10 10 \n10 10 10 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    Matrix<int> m3 = m1 + m2;
+    EXPECT_EQ(m3(0,0),10);
+    EXPECT_EQ(m3(1,1),10);
+    Matrix<double> m4({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m5({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    Matrix<double> m6 = m4 + m5;
+    EXPECT_EQ(m6(0,0), 10.0);
+    EXPECT_EQ(m6(1,1), 10.0);
 }
 TEST(MatrixOperatorMinusTest, OperatorMinus){
-    testing::internal::CaptureStdout();
-    test_operator_minus();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "8 6 4 \n2 0 -2 \n-4 -6 -8 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    Matrix<int> m3 = m2 - m1;
+    EXPECT_EQ(m3(0,0),8);
+    EXPECT_EQ(m3(1,1),0);
+    Matrix<double> m4({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m5({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    Matrix<double> m6 = m5 - m4;
+    EXPECT_EQ(m6(0,0), 8.0);
+    EXPECT_EQ(m6(1,1), 0.0);
 }
 
 
 TEST(MatrixOperatorMultiplyTest, OperatorMultiply){
-    testing::internal::CaptureStdout();
-    test_operator_multiply();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "30 24 18 \n84 69 54 \n138 114 90 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    Matrix<int> m3 = m1 * m2;
+    EXPECT_EQ(m3(0,0),30);
+    EXPECT_EQ(m3(1,1),69);
+    Matrix<double> m4({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m5({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    Matrix<double> m6 = m4 * m5;
+    EXPECT_EQ(m6(0,0), 30.0);
+    EXPECT_EQ(m6(1,1), 69.0);
+
 }
 
 TEST(MatrixOperatorScalarMultiplyTest, OperatorScalarMultiply){
-    testing::internal::CaptureStdout();
-    test_operator_scalar_multiply();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "2 4 6 \n8 10 12 \n14 16 18 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2 = m1 * 2;
+    EXPECT_EQ(m2(0,0),2);
+    EXPECT_EQ(m2(1,1),10);
+    Matrix<double> m3({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m4 = m3 * 2.0;
+    EXPECT_EQ(m4(0,0), 2.0);
+    EXPECT_EQ(m4(1,1), 10.0);
+
 }
 
 TEST(MatrixOperatorPlusEqualsTest, OperatorPlusEquals){
-    testing::internal::CaptureStdout();
-    test_operator_plus_equals();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "10 10 10 \n10 10 10 \n10 10 10 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    m1 += m2;
+    EXPECT_EQ(m1(0,0),10);
+    EXPECT_EQ(m1(1,1),10);
+    Matrix<double> m3({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m4({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    m3 += m4;
+    EXPECT_EQ(m3(0,0), 10.0);
+    EXPECT_EQ(m3(1,1), 10.0);
+
 }
 
 TEST(MatrixOperatorMinusEqualsTest, OperatorMinusEquals){
-    testing::internal::CaptureStdout();
-    test_operator_minus_equals();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "8 6 4 \n2 0 -2 \n-4 -6 -8 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    m2 -= m1;
+    EXPECT_EQ(m2(0,0),8);
+    EXPECT_EQ(m2(1,1),0);
+    Matrix<double> m3({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m4({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    m4 -= m3;
+    EXPECT_EQ(m4(0,0), 8.0);
+    EXPECT_EQ(m4(1,1), 0.0);
+
 }
 
 TEST(MatrixOperatorMultiplyEqualsTest, OperatorMultiplyEquals){
-    testing::internal::CaptureStdout();
-    test_operator_multiply_equals();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "30 24 18 \n84 69 54 \n138 114 90 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    Matrix<int> m2({{9, 8, 7}, {6, 5, 4}, {3, 2, 1}});
+    m1 *= m2;
+    EXPECT_EQ(m1(0,0),30);
+    EXPECT_EQ(m1(1,1),69);
+    Matrix<double> m3({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, {7.0, 8.0, 9.0}});
+    Matrix<double> m4({{9.0, 8.0, 7.0}, {6.0, 5.0, 4.0}, {3.0, 2.0, 1.0}});
+    m3 *= m4;
+    EXPECT_EQ(m3(0,0), 30.0);
+    EXPECT_EQ(m3(1,1), 69.0);
+
 }
 
 
 TEST(MatrixTransposeTest, Transpose){
-    testing::internal::CaptureStdout();
-    test_matrix_transpose();
-    std::string output = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(output, "1 4 \n2 5 \n3 6 \n");
+    Matrix<int> m1({{1, 2, 3}, {4, 5, 6}});
+    m1 = m1.t();
+    EXPECT_EQ(m1(0,0),1);
+    EXPECT_EQ(m1(1,1),5);
+    Matrix<double> m2({{1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}});
+    m2 = m2.t();
+    EXPECT_EQ(m2(0,0), 1.0);
+    EXPECT_EQ(m2(1,1), 5.0);
 }
+
