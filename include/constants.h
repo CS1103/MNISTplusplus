fa1fa77b08e7711f0edc8ca976626a0eaa4dafea
Mauwt <david.jacobo@utec.edu.pe>
2023-05-24 19:12:33 -0500

    Reproducible random matrix method + == and != operators
    Co-authored-by: arturombc <arturo.barrantes@utec.edu.pe>
    Co-authored-by: Mauwt <david.jacobo@utec.edu.pe>

--- include/constants.h
+++ include/constants.h
@@ -8,4 +8,8 @@ extern std::string TRAIN_IMAGE_PATH;
 extern std::string TRAIN_LABEL_PATH;
 extern std::string TEST_IMAGE_PATH;
 extern std::string TEST_LABEL_PATH;
+
+extern float random_lower_limit;
+extern float random_upper_limit;
+
 #endif //MNISTPLUSPLUS_CONSTANTS_H
--- src/constants.cpp
+++ src/constants.cpp
@@ -4,3 +4,6 @@ std::string TRAIN_IMAGE_PATH = "../data/train-images.idx3-ubyte";
 std::string TRAIN_LABEL_PATH = "../Data/train-labels.idx1-ubyte";
 std::string TEST_IMAGE_PATH = "../data/t10k-images.idx3-ubyte";
 std::string TEST_LABEL_PATH = "../Data/t10k-labels.idx1-ubyte";
+
+float random_lower_limit = 0.0;
+float random_upper_limit = 1.0;
