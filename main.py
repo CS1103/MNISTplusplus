c0a32c9fe89b037c6fcee7804a29623a78961b56
jonyyy1 <jonathan.quilca@utec.edu.pe>
2023-06-25 15:06:30 -0500

    logic for send file .json to project c++

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask import (
 )
 import os
 import time
+import subprocess
+import json
 
 carpeta_actual = os.getcwd()
 carpeta_static = carpeta_actual + "/templates/static"
@@ -20,14 +22,13 @@ def index():
 
 @app.route("/backpropagation", methods=['POST'])
 def backpropagation():
-    response = {"answer": 9}
-    try:
+    compilar = subprocess.run(["g++", carpeta_actual+"/test/inference/inference.cpp", "-o", "ejecutable"])
+    if compilar.returncode == 0:
         matriz = request.get_json()['matriz']
-        time.sleep(10)
-        print(matriz)
-    except Exception as e:
-        print(e)
-
+        with open('matriz.json', 'w') as archivo:
+                json.dump(matriz, archivo)
+                resultado = subprocess.run(["./ejecutable"], input="matriz.json", text=True, capture_output=True)
+    response = {"answer": "1"}
     return jsonify(response)
 
 
