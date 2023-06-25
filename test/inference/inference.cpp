29846ddd1746359c8643e1a0ea8c7a326075f063
Susana Chang <samanta.chang@utec.edu.pe>
2023-06-25 16:35:04 -0500

    avance inference.cpp

--- test/inference/inference.cpp
+++ test/inference/inference.cpp
@@ -4,30 +4,32 @@
 
 #include <iostream>
 #include <fstream>
-#include "../../include/constants.h"
 
-#include "../../include/neural_network.h"
+#include "constants.h"
+#include "neural_network.h"
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        throw std::invalid_argument("Invalid argument: no argument was provided");
-    }
+int main() {
+
     neural_network nn;
-    std::filesystem::path model_path = get_absolute_path("model.json");
+//    std::filesystem::path model_path = get_absolute_path("model.json");
 //    nn.loadModel("model.json");
 
-    std::filesystem::path input_path = get_absolute_path(argv[1]);
+    std::filesystem::path input_path = get_absolute_path("matrix.bin");
     Matrix<double> input_matrix(28, 28);
     std::ifstream input_file(input_path, std::ios::binary);
+
     input_matrix.deserialize(input_file);
 
 //    Matrix<double> output_matrix = nn.inference(input_matrix);
+    Matrix<double> output_matrix(10, 1);
+    output_matrix[5][0] = 1;
 
-    std::filesystem::path fileName = input_path.filename();
-    std::filesystem::path outputPath = "/outputs/" / fileName;
+    for (int i = 0; i < output_matrix.get_rows(); i++) {
+        if (output_matrix(i, 0) == 1) {
+            std::cout << i;
+            break;
+        }
+    }
 
-    std::ofstream output_file(outputPath, std::ios::binary);
-//    output_matrix.serialize(output_file);
-    std::cout << "1";
     return 0;
 }
