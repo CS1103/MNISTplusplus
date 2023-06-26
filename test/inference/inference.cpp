2dd8fba573243b730e2bc58267ca93372283d4bf
jonyyy1 <jonathan.quilca@utec.edu.pe>
2023-06-25 19:34:48 -0500

    change deserialize for the class matrix.h

--- test/inference/inference.cpp
+++ test/inference/inference.cpp
@@ -11,16 +11,32 @@
 int main() {
 
     neural_network nn;
-//    std::filesystem::path model_path = get_absolute_path("model.json");
-//    nn.loadModel("model.json");
 
-    std::filesystem::path input_path = get_absolute_path("matrix.bin");
+
+    //std::filesystem::path model_path = get_absolute_path("model.json");
+    //nn.loadModel("model.json");
+    //std::filesystem::path input_path = get_absolute_path("matrix.bin");
+
+
     Matrix<double> input_matrix(28, 28);
-    std::ifstream input_file(input_path, std::ios::binary);
-
+    std::ifstream input_file("matrix.bin", std::ios::binary);
     input_matrix.deserialize(input_file);
+    //cout << input_matrix<< endl;
 
-//    Matrix<double> output_matrix = nn.inference(input_matrix);
+
+    //Matrix<double> output_matrix = nn.inference(input_matrix);
+
+
+    for (int i = 0; i < input_matrix.get_rows(); i++) {
+        for (int j = 0; j < input_matrix.get_cols(); j++) {
+            std::cout << input_matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+    //Matrix<double> output_matrix = nn.inference(input_matrix);
     Matrix<double> output_matrix(10, 1);
     output_matrix[5][0] = 1;
 
@@ -30,6 +46,5 @@ int main() {
             break;
         }
     }
-
     return 0;
 }
